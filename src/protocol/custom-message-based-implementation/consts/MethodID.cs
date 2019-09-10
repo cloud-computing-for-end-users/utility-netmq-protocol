8ad1a824e7d854e5ea23adc3755efda007dd58d2
gaticks <kenneth.norholm@gmail.com>
2019-09-10 10:41:36 +0200

    needed  a build CC-10

--- src/protocol/custom-message-based-implementation/consts/MethodID.cs
+++ src/protocol/custom-message-based-implementation/consts/MethodID.cs
@@ -7,6 +7,6 @@ namespace custom_message_based_implementation.consts
     public class MethodID
     {
         public const string METHOD_SLAVE_OWNER_GET_SLAVE = "1-SO_REQ_GET_SLAVE";
-        public const string METHOD_SLAVE_OWNER_GET_LIST_OF_RUNNING_APP= "2-SO_REQ_GET_LIST";
+        public const string METHOD_SLAVE_OWNER_GET_LIST_OF_RUNNING_APP= "2-SO_REQ_GET_LIST"; 
     }
 }
