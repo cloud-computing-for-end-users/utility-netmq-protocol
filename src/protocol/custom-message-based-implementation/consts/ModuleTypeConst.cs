e3823a3b63fec1c045089fcd5a16e9bda82e4dd3
gaticks <kenneth.norholm@gmail.com>
2019-09-11 10:35:37 +0200

    CC-63 added module types

--- src/protocol/custom-message-based-implementation/consts/ModuleTypeConst.cs
+++ src/protocol/custom-message-based-implementation/consts/ModuleTypeConst.cs
@@ -8,5 +8,8 @@ namespace custom_message_based_implementation.consts
     {
         public const string MODULE_TYPE_SERVER_MODULE = "MOD_TYPE_SERVER_MODULE";
         public const string MODULE_TYPE_SLAVE_OWNER = "MOD_TYPE_SLAVE_OWNER";
+        public const string MODULE_TYPE_CLIENT = "MOD_TYPE_CLIENT";
+        public const string MODULE_TYPE_FILE= "MOD_TYPE_FILE";
+        public const string MODULE_TYPE_DATABASE= "MOD_TYPE_DATABASE";
     }
 }
