86b047af8001fe371c2f232b2a3963a4fe064b4f
gaticks <kenneth.norholm@gmail.com>
2019-09-20 09:20:33 +0200

    CC-63 deleted some unnessesary code

--- src/protocol/message-based-communication/module/BaseCommunicationModule.cs
+++ src/protocol/message-based-communication/module/BaseCommunicationModule.cs
@@ -13,12 +13,8 @@ namespace message_based_communication.module
         public abstract string CALL_ID_PREFIX { get; }
 
         protected ProxyHelper proxyHelper;
-        //protected ModuleID moduleID;
-        //public ModuleID ModuleID { get { return this.moduleID;  } }
         protected ModuleType moduleType;
         public ModuleType ModuleType { get { return moduleType; } }
-        //protected ConnectionInformation communicationHub; //this must point to a BaseRouterModule connection
-
         public BaseCommunicationModule(ModuleType moduleType)
         {
             this.moduleType = moduleType;
@@ -27,47 +23,9 @@ namespace message_based_communication.module
 
         public virtual void Setup(ConnectionInformation baseRouterModule, Port baseRouterRegistrationPort, ConnectionInformation forSelf, Encoding customEncoding)
         {
-            //SetupHasBeenCalled = true;
-
-            //this.communicationHub = baseRouterModule;
-                
             this.proxyHelper = new ProxyHelper();
-            //this.connectionWrapper.Setup(baseRouterModule, this);
-
-
-
-            //this.proxyHelper.ModuleID= RegisterModule(this.moduleType, baseRouterModule, baseRouterRegistrationPort, forSelf);
-
-            ////throw new NotImplementedException();
-            ///
-
             this.proxyHelper.Setup(baseRouterModule,baseRouterRegistrationPort,moduleType, forSelf, this, customEncoding);
-
         }
-
-        //private static ModuleID RegisterModule(ModuleType moduleType, ConnectionInformation baseRouterModule,Port baseRouterRegistrationPort , ConnectionInformation forSelf)
-        //{
-        //    var reqSocker = new RequestSocket("tcp://" + baseRouterModule.IP.TheIP  + ":"+ baseRouterRegistrationPort.ThePort);
-
-        //    var request = new RegisterModuleRequest()
-        //    {
-        //        CallID = new CallID() { ID = "SETUP_" + new Random().Next() },
-        //        ModuleType = moduleType,
-        //        ConnInfo = forSelf
-        //    };
-
-        //    var encodedReq = encoding.Encoding.EncodeRegisterModuleRequest(request);
-        //    reqSocker.SendMultipartMessage(encodedReq);
-
-        //    //reciving response
-        //    var encodedResponse = reqSocker.ReceiveMultipartMessage();
-        //    var decodedResponse = encoding.Encoding.TryDecodeRegisterModuleResponse(encodedResponse);
-
-
-        //    return decodedResponse.ModuleID;
-
-        //}
-
 
     }
 }
