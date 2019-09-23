ad426d07a82d3edadab4f203dbfd7e4efe996fac
gaticks <kenneth.norholm@gmail.com>
2019-09-23 11:18:10 +0200

    CC-10 testing build server

---
+++ src/protocol/custom-message-based-implementation/Tesst.cs
@@ -0,0 +1,12 @@
+﻿using message_based_communication;
+
+namespace custom_message_based_implementation
+{
+    class Tesst
+    {
+        public Tesst()
+        {
+            new Testing();
+        }
+    }
+}
