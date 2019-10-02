183733b4c02187d6690b7e543901cf67d0f7a13d
gaticks <kenneth.norholm@gmail.com>
2019-10-02 11:18:56 +0200

    CC-10

--- src/protocol/client-slave-message-communication/model/mouse-action/BaseMouseAction.cs
+++ src/protocol/client-slave-message-communication/model/mouse-action/BaseMouseAction.cs
@@ -8,7 +8,7 @@ namespace client_slave_message_communication.model.mouse_action
     /// <summary>
     /// should be treated as an abstract class and thust shound nover be nessesary to instanciate
     /// </summary>
-    public abstract class BaseMouseAction
+    public class BaseMouseAction
     {
         public enum MouseAction
         {
@@ -26,6 +26,6 @@ namespace client_slave_message_communication.model.mouse_action
         /// <summary>
         /// must always be set by <see langword="abstract"/>subclass
         /// </summary>
-        public abstract string Action { get; } 
+        public virtual string Action { get; set; } 
     }
 }
