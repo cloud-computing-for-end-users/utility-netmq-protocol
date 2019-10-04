1ff1883d122789c1ebd8b4c9bfae960ca0d89a20
gaticks <kenneth.norholm@gmail.com>
2019-10-04 09:48:01 +0200

    CC-10 idiot fix

--- src/protocol/client-slave-message-communication/model/mouse-action/RightMouseUpAction.cs
+++ src/protocol/client-slave-message-communication/model/mouse-action/RightMouseUpAction.cs
@@ -6,7 +6,7 @@ namespace client_slave_message_communication.model.mouse_action
 {
     public class RightMouseUpAction : BaseMouseAction
     {
-        public override string Action => MouseAction.RightDown.ToString();
+        public override string Action => MouseAction.RightUp.ToString();
         public RelativeScreenLocation RelativeScreenLocation { get; set; }
     }
 }
