51f423e1b614c401fab3a2fc34789dbbe4623463
Krystof Spiller <253812@via.dk>
2019-10-24 09:15:27 +0200

    CC-106 File servermodule request, interface, proxy and model classes

---
+++ src/protocol/custom-message-based-implementation/interfaces/IFileServermodule.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using custom_message_based_implementation.model;
+
+namespace custom_message_based_implementation.interfaces
+{
+    public interface IFileServermodule
+    {
+        List<FileName> GetListOfFiles(PrimaryKey pk);
+        void UploadFile(File file, PrimaryKey pk, bool overwrite);
+        File DownloadFile(FileName fileName, PrimaryKey pk);
+    }
+}
